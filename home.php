f5a3842edefc2d5929addeddcf2d244c1497f3f9
João Pedro <112526011+JohnPdro@users.noreply.github.com>
2023-05-02 22:16:49 -0300

    primeiro codigo php

---
+++ home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Primeiro arquivo PHP</h1>
+    <p>Olá mundo! no html</p>
+    <?php
+     echo "Hello, World! no php"
+     ?>
+</body>
+</html>
